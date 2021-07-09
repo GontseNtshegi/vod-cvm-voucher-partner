3b3100cf63c5215b5504431ca20ff9e68f835b7c
Gontse Ntshegi <gontse.ntshegi@vodacom.co.za>
2021-07-09 12:17:44 +0200

    Only make crud API's visible with the crud profile

--- src/main/java/za/co/vodacom/cvm/web/rest/VPFileLoadResource.java
+++ src/main/java/za/co/vodacom/cvm/web/rest/VPFileLoadResource.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.jhipster.web.util.HeaderUtil;
@@ -24,6 +25,7 @@ import za.co.vodacom.cvm.web.rest.errors.BadRequestAlertException;
  */
 @RestController
 @RequestMapping("/api")
+@Profile("crud")
 public class VPFileLoadResource {
 
     private final Logger log = LoggerFactory.getLogger(VPFileLoadResource.class);

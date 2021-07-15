f0e6c78bd4d4fa1949b632e6ee985ab46fbad953
Gontse Ntshegi <gontse.ntshegi@vodacom.co.za>
2021-07-15 18:32:25 +0200

    Increase code average to 20%

--- .build/Jenkinsfile.groovy
+++ .build/Jenkinsfile.groovy
@@ -21,7 +21,7 @@ targetNamespace			: "cvm-loyalty-voucher",            	// Edit here
 targetDeploymentConfig	: "voucher-partner",        		// Edit here N.B Ensure Image Stream for project exists on openshift
 targetBuildConfig		: "voucher-partner",             	// Edit here N.B Ensure Buildconfig
 sonarQubeUrl    		: 'https://sonarqube.orbit.vodacom.aws.corp/',
-codeCoverage			: 0
+codeCoverage			: 20
 
 ]
 

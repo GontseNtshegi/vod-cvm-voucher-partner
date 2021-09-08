ce1874b383f1f413d2d72ec90f04e45d093a41f5
Gontse Ntshegi <gontse.ntshegi@vodacom.co.za>
2021-09-08 19:18:57 +0200

    Add proxy

--- src/main/java/za/co/vodacom/cvm/config/FeignConfiguration.java
+++ src/main/java/za/co/vodacom/cvm/config/FeignConfiguration.java
@@ -1,10 +1,14 @@
 package za.co.vodacom.cvm.config;
 
+import feign.Client;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @Configuration
 @EnableFeignClients(basePackages = "za.co.vodacom.cvm")
@@ -17,5 +21,12 @@ public class FeignConfiguration {
     @Bean
     feign.Logger.Level feignLoggerLevel() {
         return feign.Logger.Level.BASIC;
+    }
+
+    @Bean
+    public Client feignClient() {
+        return new Client.Proxied(null, null,
+            new Proxy(Proxy.Type.HTTP,
+                new InetSocketAddress("http://vcacmang%40vodacom:V0d4f0n3%402020@zamdh13001p1.vodacom.corp", 8080)));
     }
 }

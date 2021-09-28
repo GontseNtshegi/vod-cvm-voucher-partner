88c9b72ba15d7221c20fa149d6a7f5cb95dd46e6
Gontse Ntshegi <gontse.ntshegi@vodacom.co.za>
2021-09-28 11:29:03 +0200

    Removed proxy

--- src/main/java/za/co/vodacom/cvm/config/FeignConfiguration.java
+++ src/main/java/za/co/vodacom/cvm/config/FeignConfiguration.java
@@ -1,15 +1,11 @@
 package za.co.vodacom.cvm.config;
 
-import feign.Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 
 @Configuration
 @EnableFeignClients(basePackages = "za.co.vodacom.cvm")
@@ -33,10 +29,10 @@ public class FeignConfiguration {
         return feign.Logger.Level.BASIC;
     }
 
-    @Bean
+    /*@Bean
     public Client feignClient() {
         return new Client.Proxied(null, null,
             new Proxy(Proxy.Type.HTTP,
                 new InetSocketAddress(proxyHost, proxyPort)));
-    }
+    }*/
 }

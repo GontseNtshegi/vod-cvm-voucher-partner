4b0902a9c3c7fd2dff2565753386d49619b1c247
Gontse Ntshegi <gontse.ntshegi@vodacom.co.za>
2022-04-21 13:15:49 +0200

    RSA encryption

--- src/main/java/za/co/vodacom/cvm/utils/RSAEncryption.java
+++ src/main/java/za/co/vodacom/cvm/utils/RSAEncryption.java
@@ -2,18 +2,24 @@ package za.co.vodacom.cvm.utils;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RSAEncryption {
+
+    private static String ivParameter = "1234567890123456";
 
     public String encrypt(String code, String key)
         throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
@@ -28,12 +34,42 @@ public class RSAEncryption {
     public String decrypt(String encryptedtext, String key)
         throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         byte[] KeyData = key.getBytes();
-        SecretKeySpec KS = new SecretKeySpec(KeyData, "RSA");
+        SecretKeySpec KS = new SecretKeySpec(KeyData, "AES");
         byte[] ecryptedtexttobytes = Base64.getDecoder().decode(encryptedtext);
-        Cipher cipher = Cipher.getInstance("RSA/None/OAEPWITHSHA-256ANDMGF1PADDING");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, KS);
         byte[] decrypted = cipher.doFinal(ecryptedtexttobytes);
         String decryptedString = new String(decrypted, Charset.forName("UTF-8"));
         return decryptedString;
     }
+
+    // encryption
+    public String encrypt2(String sSrc, String sKey) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        byte[] raw = sKey.getBytes();
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes()); // using CBC mode, you need a vector IV to increase the strength of encryption algorithm
+        // Python code to create random 16 byte iv string: iv = Random.new().read(16)
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+        byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
+        return Base64.getEncoder().encodeToString(encrypted); // use Base64 for transcoding here.
+    }
+
+    // decryption
+    public String decrypt2(String sSrc, String sKey) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+
+        byte[] raw = sKey.getBytes("ASCII");
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        byte[] encrypted1 = Base64.getDecoder().decode(sSrc.getBytes()); // decrypt with Base64 first
+        // IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
+        IvParameterSpec iv = new IvParameterSpec(Arrays.copyOfRange(encrypted1, 0, 16));
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+        // byte[] original = cipher.doFinal(encrypted1);
+        byte[] original = cipher.doFinal(Arrays.copyOfRange(encrypted1, 16, encrypted1.length));
+        String originalString = new String(original, "utf-8");
+        return originalString;
+
+    }
+
 }

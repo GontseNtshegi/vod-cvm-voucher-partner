14d3e386969d64dfd27e0dff6b88626a9a418619
Litha Fudumele <litha.fudumele@vodacom.co.za>
2022-05-25 13:50:20 +0200

    Remove voucher validity check for this release(still to be tested)

--- src/main/java/za/co/vodacom/cvm/web/rest/VoucherServiceImpl.java
+++ src/main/java/za/co/vodacom/cvm/web/rest/VoucherServiceImpl.java
@@ -243,7 +243,7 @@ public class VoucherServiceImpl implements VoucherApiDelegate {
                                         }
                                         //set response
                                         voucherAllocationResponse.setCollectPoint(vpVoucherDef.getVendor());
-                                        voucherAllocationResponse.setExpiryDate(ZonedDateTime.parse(couponsResponse.getCoupon().getRedeemToDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'+'SSSS").withZone(ZoneId.systemDefault())).toOffsetDateTime());
+                                        voucherAllocationResponse.setExpiryDate(expiryDate);
                                         voucherAllocationResponse.setTrxId(voucherAllocationRequest.getTrxId());
                                         voucherAllocationResponse.setVoucherCategory(vpVoucherDef.getCategory());
                                         voucherAllocationResponse.setVoucherCode(voucherCode);

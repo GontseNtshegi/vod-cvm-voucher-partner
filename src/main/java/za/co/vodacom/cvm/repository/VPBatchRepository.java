311f033512c5dcd05c7187557064d3396c8fdcfd
Shack Tsotetsi <shack.tsotetsi@vodacom.co.za>
2023-04-11 20:28:11 +0200

    Added BatchDTO

--- src/main/java/za/co/vodacom/cvm/repository/VPBatchRepository.java
+++ src/main/java/za/co/vodacom/cvm/repository/VPBatchRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import za.co.vodacom.cvm.domain.VPBatch;
 import za.co.vodacom.cvm.service.dto.product.Product;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 /**
@@ -14,9 +15,41 @@ import java.util.Optional;
 @SuppressWarnings("unused")
 @Repository
 public interface VPBatchRepository extends JpaRepository<VPBatch, Long> {
+//    @Query(
+//        value = "select " +
+//            "d.product_id," +
+//            " d.type, " +
+//            "d.description, " +
+//            "v.description as voucher_desc, " +
+//            "f.file_name, " +
+//            "count(*) ," +
+//            " v.start_date," +
+//            " v.end_date, " +
+//            "v.expiry_date " +
+//            "from vp_vouchers v, vp_voucher_def d, vp_file_load f " +
+//            "where v.product_id=d.product_id " +
+//            "and v.file_id=f.id " +
+//            "and v.batch_id=:id " +
+//            "and issued_date is null and end_date > now() group by product_id,d.type,d.description,v.description, f.file_name,v.start_date,v.end_date,v.expiry_date order by 1",
+//        nativeQuery = true
+//    )
     @Query(
-        value = "select d.product_id, d.type, d.description, v.description as voucher_desc, f.file_name, count(*) , v.start_date, v.end_date, v.expiry_date from vp_vouchers v, vp_voucher_def d, vp_file_load f where v.product_id=d.product_id and v.file_id=f.id and v.batch_id=:id and issued_date is null and end_date > now() group by product_id,d.type,d.description,v.description, f.file_name,v.start_date,v.end_date,v.expiry_date order by 1",
-        nativeQuery = true
+        value ="select new za.co.vodacom.cvm.service.dto.batch.BatchDetailsDTO(d.id, " +
+            "d.type," +
+            "d.description," +
+            "v.description ," +
+            "v.startDate, "+
+            "v.endDate,  "+
+            "v.expiryDate , "+
+            "f.fileName )"+
+            "from VPVouchers v, VPVoucherDef d, VPFileLoad f " +
+            "where v.id = d.id " +
+            "and v.fileId=f.id " +
+            "and v.batchId=:id " +
+            "and v.issuedDate is null " +
+            "and v.endDate > :sysdate " +
+            "group by d.id,d.type,d.description,v.description,f.fileName,v.startDate,v.endDate,v.expiryDate " +
+            "order by 1"
     )
-    Optional<VPBatch> getVoucherQuantity(@Param("id") Long id);
+    Optional<VPBatch> getVoucherQuantity(@Param("id") Long id,@Param("sysdate") ZonedDateTime sysdate);
 }
---
+++ src/main/java/za/co/vodacom/cvm/service/dto/batch/BatchDetailsDTO.java
@@ -0,0 +1,109 @@
+package za.co.vodacom.cvm.service.dto.batch;
+
+import java.time.ZonedDateTime;
+
+public class BatchDetailsDTO {
+
+    private String id;
+
+    private String type;
+
+    private String description;
+
+    private String voucherDescription;
+
+    private ZonedDateTime startDate;
+    private ZonedDateTime endDate;
+    private ZonedDateTime expiryDate;
+
+    private String fileName;
+
+    public BatchDetailsDTO(String id, String type, String description, String voucherDescription, ZonedDateTime startDate, ZonedDateTime endDate, ZonedDateTime expiryDate, String fileName) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+        this.voucherDescription = voucherDescription;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.expiryDate = expiryDate;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVoucherDescription() {
+        return voucherDescription;
+    }
+
+    public void setVoucherDescription(String voucherDescription) {
+        this.voucherDescription = voucherDescription;
+    }
+
+    public ZonedDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(ZonedDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public ZonedDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(ZonedDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public ZonedDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(ZonedDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public String toString() {
+        return "BatchDetailsDTO{" +
+            "id='" + id + '\'' +
+            ", type='" + type + '\'' +
+            ", description='" + description + '\'' +
+            ", voucherDescription='" + voucherDescription + '\'' +
+            ", startDate=" + startDate +
+            ", endDate=" + endDate +
+            ", expiryDate=" + expiryDate +
+            ", fileName='" + fileName + '\'' +
+            '}';
+    }
+}

7f3394980df83791af6b4f19f40d3e4cb5aebbc8
Shack Tsotetsi <shack.tsotetsi@vodacom.co.za>
2023-04-11 14:16:29 +0200

    bachlist logic implementation

--- src/main/java/za/co/vodacom/cvm/web/rest/BatchServiceImpl.java
+++ src/main/java/za/co/vodacom/cvm/web/rest/BatchServiceImpl.java
@@ -1,44 +1,46 @@
 package za.co.vodacom.cvm.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import za.co.vodacom.cvm.domain.VPBatch;
 import za.co.vodacom.cvm.service.VPBatchService;
-import za.co.vodacom.cvm.web.api.ApiUtil;
 import za.co.vodacom.cvm.web.api.BatchApiDelegate;
 import za.co.vodacom.cvm.web.api.model.BatchListResponseObject;
-
-import javax.xml.datatype.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class BatchServiceImpl implements BatchApiDelegate {
 
+    public static final Logger log = LoggerFactory.getLogger(BatchServiceImpl.class);
     @Autowired
     VPBatchService vpBatchService;
     @Override
-    public ResponseEntity<List<BatchListResponseObject>> batchlist(Integer period) {
-
+    public ResponseEntity<List<BatchListResponseObject>> batchlist() {
        List<BatchListResponseObject> batchListResponseObjects = new ArrayList<>();
-
        vpBatchService.getAll()
            .ifPresent(vpBatches -> {
                vpBatches.forEach(vpBatch -> {
-                   // ToDo to add other fields once db changes are made
                    BatchListResponseObject batchListResponseObject = new BatchListResponseObject();
                    batchListResponseObject.setBatchSeq(vpBatch.getId().intValue());
                    batchListResponseObject.setBatchComment(vpBatch.getComment());
                    batchListResponseObject.setCreateDate(vpBatch.getCreateDate().toOffsetDateTime());
+                   batchListResponseObject.setBatchName(vpBatch.getName());
+                   batchListResponseObject.setStatus(vpBatch.getStatus());
+                   batchListResponseObject.setActivateUser(vpBatch.getActivateUser());
+                   batchListResponseObject.setLoadDate(vpBatch.getLoadDate().toOffsetDateTime());
+                   batchListResponseObject.setCreateUser(vpBatch.getCreateUser());
+
+
                    batchListResponseObjects.add(batchListResponseObject);
                });
            });
+
+        log.debug("BatchList {} ",batchListResponseObjects);
         return new ResponseEntity<>(batchListResponseObjects,HttpStatus.OK);
-
     }
-
 }

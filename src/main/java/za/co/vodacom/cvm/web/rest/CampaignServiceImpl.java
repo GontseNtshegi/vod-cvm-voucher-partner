fca4de49886e9e884d77eba41b85eead4a8ac871
Ngqabutho Mpofu <trustnmpofu@gmail.com>
2023-05-03 12:12:23 +0200

    Bug fix

--- src/main/java/za/co/vodacom/cvm/web/rest/CampaignServiceImpl.java
+++ src/main/java/za/co/vodacom/cvm/web/rest/CampaignServiceImpl.java
@@ -67,8 +67,8 @@ public class CampaignServiceImpl  implements CampaignApiDelegate {
             campaignsList.add(new CampaignListResponseObject()
                 .campaignId(vpcampaign.getId().toString())
                 .campaignName(vpcampaign.getName())
-                .startDate(vpcampaign.getStartDate().toLocalDate())
-                .endDate(vpcampaign.getEndDate() == null? null:vpcampaign.getEndDate().toLocalDate()));
+                .startDate(vpcampaign.getStartDate().toLocalDateTime().toString())
+                .endDate(vpcampaign.getEndDate() == null? null:vpcampaign.getEndDate().toLocalDateTime().toString()));
         }
         log.debug("Campaign List{}", campaignsList.toString());
         return new ResponseEntity<>(campaignsList, HttpStatus.OK);

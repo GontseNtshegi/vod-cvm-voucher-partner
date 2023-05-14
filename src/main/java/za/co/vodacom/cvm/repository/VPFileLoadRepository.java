68bdecf41d5f722f2dd53c30cee28c6b8c361908
Shack Tsotetsi <shack.tsotetsi@vodacom.co.za>
2023-05-14 23:24:26 +0200

    updated queries to use JPQL

--- src/main/java/za/co/vodacom/cvm/repository/VPFileLoadRepository.java
+++ src/main/java/za/co/vodacom/cvm/repository/VPFileLoadRepository.java
@@ -14,8 +14,7 @@ import java.util.Optional;
 @Repository
 public interface VPFileLoadRepository extends JpaRepository<VPFileLoad, Long> {
     @Query(
-        value = "select * from vp_file_load where batch_id=:batchId and file_name=:fileName",
-        nativeQuery = true
+        value = "select v from VPFileLoad v where v.batchId=:batchId and v.fileName=:fileName"
     )
     Optional<VPFileLoad> getFileByNameAndId(@Param("batchId") Integer batchId, @Param("fileName") String fileName);
 }

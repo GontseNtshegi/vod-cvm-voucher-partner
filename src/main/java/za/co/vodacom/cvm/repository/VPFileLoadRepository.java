4279ab564f5912447cb9a7ebba6fa5e031cbcfbb
Marli Louwrens <Marli.Louwrens@vcontractor.co.za>
2023-05-18 12:45:31 +0200

    Revert "Bugfix - Merge Conflict"
    This reverts commit 53b94037fb80dfd6df460abdf4fd80c7b2a9e7bc, reversing
    changes made to 6343a320f899e00ce83f10b57804c981e783aaeb.

--- src/main/java/za/co/vodacom/cvm/repository/VPFileLoadRepository.java
+++ src/main/java/za/co/vodacom/cvm/repository/VPFileLoadRepository.java
@@ -1,17 +1,12 @@
 package za.co.vodacom.cvm.repository;
 
 import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import za.co.vodacom.cvm.domain.VPFileLoad;
-
-import java.util.Optional;
 
 /**
  * Spring Data SQL repository for the VPFileLoad entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface VPFileLoadRepository extends JpaRepository<VPFileLoad, Long> {
-    Optional<VPFileLoad> findByBatchIdAndAndFileName(Integer batchId , String fileName);
-}
+public interface VPFileLoadRepository extends JpaRepository<VPFileLoad, Long> {}

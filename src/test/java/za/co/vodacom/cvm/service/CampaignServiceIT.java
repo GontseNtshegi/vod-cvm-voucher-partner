2e65b0be7a8c4d8bb3ee7f67b8b4f2afff5ce10b
mpofn001 <ngqabutho.mpofu@vodacom.co.za>
2024-03-07 12:08:08 +0200

    tests

--- src/test/java/za/co/vodacom/cvm/service/CampaignServiceIT.java
+++ src/test/java/za/co/vodacom/cvm/service/CampaignServiceIT.java
@@ -5,6 +5,7 @@ import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import za.co.vodacom.cvm.IntegrationTest;
@@ -12,10 +13,14 @@ import za.co.vodacom.cvm.config.Constants;
 import za.co.vodacom.cvm.domain.VPCampaign;
 import za.co.vodacom.cvm.domain.VPCampaignVouchers;
 import za.co.vodacom.cvm.service.dto.campaign.CampaignProductDTO;
+import za.co.vodacom.cvm.service.dto.campaign.QuantityDetailsDTO;
+import za.co.vodacom.cvm.web.api.model.LinkDelinkRequest;
+import za.co.vodacom.cvm.web.rest.TestUtil;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +41,9 @@ class CampaignServiceIT {
     public static final long DEFAULT_ID = 5L;
     private static final String DEFAULT_CAMPAIGN_ID = "21";
     private static final String DEFAULT_STRING = "AAAAAA";
+    private static final String DEFAULT_STRINGS_ADD = "CDA216,EXP626";
+    private static final String DEFAULT_STRINGS_RMV = "VCS213,WU450,GFT321";
+    private static final Integer DEFAULT_INT = 2;
     @MockBean
     VPCampaignService vpCampaignService;
     @MockBean
@@ -48,7 +56,7 @@ class CampaignServiceIT {
     private MockMvc restMockMvc;
 
 
-
+//////////////////////////Tests
     private List<VPCampaign> createCampaignList() {
         VPCampaign vpCampaign = new VPCampaign();
         List<VPCampaign> campaignList = new ArrayList<>();
@@ -72,7 +80,7 @@ class CampaignServiceIT {
     private VPCampaignVouchers createCampaignVoucher() {
         VPCampaignVouchers campaignVouchers = new VPCampaignVouchers();
         campaignVouchers.setId(DEFAULT_ID);
-        campaignVouchers.setActiveYN(Constants.YES);
+        campaignVouchers.setActiveYN(Constants.NO);
         campaignVouchers.setCampaignId(Long.valueOf(DEFAULT_CAMPAIGN_ID));
         campaignVouchers.setCreateDate(ZonedDateTime.now().minusMonths(6));
         campaignVouchers.setModifiedDate(ZonedDateTime.now());
@@ -92,10 +100,32 @@ class CampaignServiceIT {
         return list;
     }
 
+
+    private List<QuantityDetailsDTO> createQuantityDetailsDto() {
+        QuantityDetailsDTO quantityDetailsDTO = new QuantityDetailsDTO();
+        List<QuantityDetailsDTO> dtoList = new ArrayList<>();
+        quantityDetailsDTO.setCount(DEFAULT_ID);
+        quantityDetailsDTO.setDescription(DEFAULT_STRING);
+        quantityDetailsDTO.setEndDate(null);
+        quantityDetailsDTO.setExpiryDate(null);
+        quantityDetailsDTO.setProductDescription(DEFAULT_STRING);
+        quantityDetailsDTO.setProductId(DEFAULT_STRING);
+        quantityDetailsDTO.setType(DEFAULT_STRING);
+        dtoList.add(quantityDetailsDTO);
+        return  dtoList;
+    }
+
+    private LinkDelinkRequest createLinkDelinkRequest(boolean isAddOnly, boolean isBoth) {
+        LinkDelinkRequest request = new LinkDelinkRequest();
+            request.setAddProducts(DEFAULT_STRINGS_ADD);
+            request.setRemoveProducts(DEFAULT_STRINGS_RMV);
+        return  request;
+    }
+
     ////////////////////////////////////////////////////////////// TESTS
 
     @Test
-    void getCampaignList() throws Exception{
+    void getCampaignList() throws Exception {
         given(this.vpCampaignService.findAll())
             .willReturn(createCampaignList());
         restMockMvc.perform(get("/api/campaign/list"))
@@ -105,7 +135,7 @@ class CampaignServiceIT {
     }
 
     @Test
-    void getCampaignProducts() throws Exception{
+    void getCampaignProducts() throws Exception {
         given(this.vpCampaignService.findOne(ArgumentMatchers.any()))
             .willReturn(Optional.of(createCampaign()));
         given(this.vpCampaignVouchersService.findOne(ArgumentMatchers.any()))
@@ -118,7 +148,7 @@ class CampaignServiceIT {
     }
 
     @Test
-    void getCampaignProductsNoProducts() throws Exception{
+    void getCampaignProductsNoProducts() throws Exception {
         given(this.vpCampaignService.findOne(ArgumentMatchers.any()))
             .willReturn(Optional.of(createCampaign()));
         given(this.vpCampaignVouchersService.findOne(ArgumentMatchers.any()))
@@ -129,18 +159,47 @@ class CampaignServiceIT {
     }
 
     @Test
-    void getCampaignProductsNoCampaign() throws Exception{
+    void getCampaignProductsNoCampaign() throws Exception {
         restMockMvc.perform(get("/api/campaign/products/{1}"
                 ,DEFAULT_CAMPAIGN_ID))
             .andExpect(status().isNotFound());
     }
 
     @Test
-    void getQuantities() {
+    void getQuantities() throws Exception {
+        given(this.vpCampaignService.findOne(ArgumentMatchers.any()))
+            .willReturn(Optional.of(createCampaign()));
+        given(this.vpVouchersService.getVoucherQuantity(ArgumentMatchers.any(),ArgumentMatchers.any()))
+            .willReturn(createQuantityDetailsDto());
+        restMockMvc.perform(get("/api/campaign/vouchers/quantities/{1}"
+                ,DEFAULT_CAMPAIGN_ID))
+            .andExpect(status().isOk());
     }
 
-@Test
-    void linkDelinkProduct() {
+    @Test
+    void getQuantitiesNoCampaign() throws Exception {
+        restMockMvc.perform(get("/api/campaign/vouchers/quantities/{1}"
+                ,DEFAULT_CAMPAIGN_ID))
+            .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    void linkDelinkProduct() throws Exception {
+        given(this.vpCampaignService.findOne(ArgumentMatchers.any()))
+            .willReturn(Optional.of(createCampaign()));
+        given(this.voucherDefService.getVouchersByProductId(ArgumentMatchers.any()))
+            .willReturn(DEFAULT_INT);
+        given(this.vpCampaignVouchersService.getVouchersByCampaign(ArgumentMatchers.any()))
+            .willReturn(List.of(createCampaignVoucher()));
+        given(this.vpCampaignVouchersService.findVoucherByProductIdandCampaignId(ArgumentMatchers.any(),ArgumentMatchers.any()))
+            .willReturn(Optional.of(createCampaignVoucher()));
+        given(this.vpCampaignVouchersService.save(ArgumentMatchers.any()))
+            .willReturn(createCampaignVoucher());
+        restMockMvc.
+            perform(put("/api/campaign/products/{1}"
+                ,DEFAULT_CAMPAIGN_ID).contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(createLinkDelinkRequest(true,false))))
+            .andExpect(status().isOk());
     }
 
 }
